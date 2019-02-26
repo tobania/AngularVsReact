8aa0b133c130a501f48f50247fc7249817c62256
Tim Neyens <tim.neyens@tobania.be>
2019-02-26 20:06:34 +0100

    Minor changes to react app

---
+++ React/Normal/AngularVsReact/src/Components/Pages/Todos.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Todo from '../Todo/Todo';
+import NewTodo from '../Todo/NewTodo';
+import GetTodos from '../../HttpServices/Todos/GetTodosService';
+import ToggleTodoItem from '../../Services/ToggleTodoItemService';
+import AddTodoItem from '../../Services/AddTodoItemService';
+
+export default class Todos extends Component {
+  constructor() {
+    super();
+    this.state = { todos: [] };
+
+    this.addCreatedTodo = this.addCreatedTodo.bind(this);
+    this.addCreatedTodoItem = this.addCreatedTodoItem.bind(this);
+    this.toggleTodoItem = this.toggleTodoItem.bind(this);
+    this.showNewTodo = this.showNewTodo.bind(this);
+    this.showTodos = this.showTodos.bind(this);
+  }
+
+  componentDidMount() {
+    GetTodos(this.showTodos);
+  }
+
+  showTodos(todos) {
+    this.setState({ todos });
+  }
+
+  addCreatedTodo(todo) {
+    const { todos } = this.state;
+    this.setState({ todos: [...todos, todo], showNew: false });
+  }
+
+  addCreatedTodoItem(id, todoItem) {
+    const { todos } = this.state;
+    const updatedTodos = AddTodoItem(todos, id, todoItem);
+
+    this.setState({ todos: updatedTodos });
+  }
+
+  toggleTodoItem(todoId, todoItemId) {
+    const { todos } = this.state;
+    const updatedTodos = ToggleTodoItem(todos, todoId, todoItemId);
+    this.setState({ todos: updatedTodos });
+  }
+
+  showNewTodo() {
+    this.setState({ showNew: true });
+  }
+
+  render() {
+    const { todos, showNew } = this.state;
+    return (
+      <div className="row mt-4">
+        {
+          todos.map(todo => (
+            <Todo
+              key={todo.identifier}
+              addCreatedTodoItem={this.addCreatedTodoItem}
+              toggleTodoItem={this.toggleTodoItem}
+              {...todo}
+            />
+          ))
+        }
+        {
+          showNew
+          && <NewTodo addCreatedTodo={this.addCreatedTodo} />
+        }
+      </div>
+    );
+  }
+}
